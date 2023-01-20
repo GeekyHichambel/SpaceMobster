5d4d0d324de03247aa36de11686a7401bcb7d951
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2023-01-20 16:33:37 +0530

    minor changes for performance
    images file

--- images.py
+++ images.py
@@ -1,8 +1,10 @@
-#Image loading for game
+#Image loading
 import pygame as pg
 import os
 
 rocket = pg.image.load(os.path.join("resources","rocket.png"))
+rocket1 = pg.image.load(os.path.join("resources","rocket1.png"))
+rocket2 = pg.image.load(os.path.join("resources","rocket2.png"))
 asteroid = pg.image.load(os.path.join("resources","asteroid.png"))
 background = pg.image.load(os.path.join("resources","BG.jpg"))
 control_button = pg.image.load(os.path.join("resources","Controlbut.png"))
@@ -12,6 +14,20 @@ start_button_2 = pg.image.load(os.path.join("resources","Startbut(2).png"))
 control_screen = pg.image.load(os.path.join("resources","controlpage.jpg"))
 game_over = pg.image.load(os.path.join("resources","game_over.png"))
 main_menu = pg.image.load(os.path.join("resources","main_menu.jpg"))
+
+def perf():
+	rocket.convert()
+	rocket1.convert()
+	rocket2.convert()
+	asteroid.convert()
+	background.convert()
+	control_button.convert()
+	control_button_2.convert()
+	start_button.convert()
+	start_button_2.convert()
+	control_screen.convert()
+	game_over.convert()
+	main_menu.convert()
 
 #Image holder for buttons
 current_control = control_button
