ecfbdcbb14248881c412e2e9464e859d517688be
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2023-01-22 12:12:29 +0530

    Add files via upload

---
+++ images.py
@@ -0,0 +1,57 @@
+#Image loading
+import pygame as pg
+import os
+
+rocket = pg.image.load(os.path.join("resources","rocket.png"))
+rocket1 = pg.image.load(os.path.join("resources","rocket1.png"))
+rocket2 = pg.image.load(os.path.join("resources","rocket2.png"))
+asteroid = pg.image.load(os.path.join("resources","asteroid.png"))
+background = pg.image.load(os.path.join("resources","BG.png"))
+control_button = pg.image.load(os.path.join("resources","Controlbut.png"))
+control_button_2 = pg.image.load(os.path.join("resources","Controlbut(2).png"))
+start_button = pg.image.load(os.path.join("resources","Startbut.png"))
+start_button_2 = pg.image.load(os.path.join("resources","Startbut(2).png"))
+control_screen = pg.image.load(os.path.join("resources","controlpage.jpg"))
+game_over = pg.image.load(os.path.join("resources","game_over.png"))
+main_menu = pg.image.load(os.path.join("resources","main_menu.jpg"))
+
+def perf():
+	rocket.convert_alpha()
+	rocket1.convert_alpha()
+	rocket2.convert_alpha()
+	asteroid.convert_alpha()
+	background.convert_alpha()
+	control_button.convert_alpha()
+	control_button_2.convert_alpha()
+	start_button.convert_alpha()
+	start_button_2.convert_alpha()
+	control_screen.convert_alpha()
+	game_over.convert_alpha()
+	main_menu.convert_alpha()
+
+class back():
+
+	def __init__(self,image):
+		self.image1 = image
+		self.image2 = image
+		self.rect1 = self.image1.get_rect(topleft=(0,0))
+		self.rect2 = self.image2.get_rect(topleft=(0,-720))
+
+	def moving_bg(self,screen,vel):
+		if self.rect2.y >= 0:
+			self.rect1.y = 0
+			self.rect2.y = -720
+			screen.blit(self.image1,(self.rect1.x,self.rect1.y))
+			screen.blit(self.image2,(self.rect2.x,self.rect2.y))
+		
+		else:
+			self.rect1.y += vel
+			self.rect2.y += vel
+			screen.blit(self.image1,(self.rect1.x,self.rect1.y))
+			screen.blit(self.image2,(self.rect2.x,self.rect2.y))
+
+#Image holder for buttons
+current_control = control_button
+current_start = start_button
+#background
+BACK = back(background)
---
+++ rocket.py
@@ -0,0 +1,66 @@
+#Rocket_object
+import pygame as pg
+import images as ig
+import config
+
+class ROCKET():
+	def __init__(self,x,y,size):
+		self.image = ig.rocket
+		self.x = x
+		self.y = y
+		self.size = size
+		self.image = pg.transform.scale(self.image,((100*self.size), (100*self.size)))
+		self.mask = pg.mask.from_surface(self.image)
+		self.vel = 8
+
+	def draw(self,screen):
+		screen.blit(self.image,(self.x, self.y))
+
+	def animate(self,screen,sprite):
+		self.image = moving_rocket[int(sprite)]
+		self.image = pg.transform.scale(self.image,((100*self.size), (100*self.size)))
+		screen.blit(self.image,(self.x, self.y))
+
+	def move(self,direction):
+		if direction == 1 and (self.x + self.vel) > 0:
+			self.x -= self.vel
+
+		elif direction == 2 and (self.x + self.vel) < 1180:
+			self.x += self.vel		
+
+class func():
+
+	# - - for plotting lifes - - #
+	def restart(self):
+		config.lifes = 3
+		config.switch = 1
+		lifes.append(life1)
+		lifes.append(life2)
+		lifes.append(life3)
+
+	def reset(self):
+		config.asteroid_list.clear()
+		lifes.clear()
+		ship.x = 600
+		ship.y = 600
+		config.moving_left = False
+		config.moving_right = False
+		config.stage = 0
+		config.score = 0
+		config.asteroid_vel = 1
+		config.asteroid_num = 10
+		config.switch = 1
+		config.y_max = -2500
+		config.y_min = -200
+
+#objects
+res = func()
+moving_rocket = [ig.rocket,ig.rocket1,ig.rocket2,ig.rocket1,ig.rocket2]
+ship = ROCKET(600,600,1)
+lifes = []
+life1 = ROCKET(0,10,0.7)
+life2 = ROCKET(70,10,0.7)
+life3 = ROCKET(140,10,0.7)
+lifes.append(life1)
+lifes.append(life2)
+lifes.append(life3)
