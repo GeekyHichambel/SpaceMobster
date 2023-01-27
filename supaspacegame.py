19b0251f5c78b313f81d36e6c658813658e0e713
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2023-01-27 22:18:49 +0530

    v2.2.0
    main game file

--- supaspacegame.py
+++ supaspacegame.py
@@ -5,7 +5,7 @@ from GAME_FUNC import scene
 import config
 import music
 import images as ig
-#screen_setup
+#screen
 screen_width = 1280
 screen_height = 720
 screen = pg.display.set_mode((screen_width,screen_height))
@@ -21,7 +21,7 @@ while config.game_state:
 
 	if config.switch == 1:
 		scene.main_menu(screen)
-
+		
 	elif config.switch == 2:
 		scene.control_menu(screen)
 
@@ -29,4 +29,4 @@ while config.game_state:
 		scene.main_game(screen)
 
 	elif config.switch == 4:
-		scene.game_over(screen)
+		scene.game_over(screen)	
