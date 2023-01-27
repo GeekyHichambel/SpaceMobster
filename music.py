2ef5b6819f57a9e49beea400ca9524a9a6849b5b
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2023-01-27 22:20:57 +0530

    v2.2.0
    music loader file

--- music.py
+++ music.py
@@ -4,13 +4,17 @@ import os
 from pygame import mixer
 import config
 
+pg.mixer.pre_init(44100, -16, 2, 512)
 pg.init()
+pg.mixer.init()
 #music resources
 switch_sfx = pg.mixer.Sound(os.path.join("resources","switch__.wav"))
 selector_sfx = pg.mixer.Sound(os.path.join("resources","selector.mp3"))
 life_lost_sfx = pg.mixer.Sound(os.path.join("resources","life_lost.wav"))
 game_over_sfx = pg.mixer.Sound(os.path.join("resources","game_over.wav"))
-#music functions
+life_gain_sfx = pg.mixer.Sound(os.path.join("resources","lifegain.wav"))
+wipeout_sfx = pg.mixer.Sound(os.path.join("resources","wipeout.wav"))
+#functions
 def load():
 	mixer.music.load(os.path.join("resources","CYBERPUNK.mp3"))
 	mixer.music.play(-1)
@@ -23,7 +27,9 @@ def volume_set():
 	switch_sfx.set_volume(config.volume)
 	selector_sfx.set_volume(config.volume)
 	life_lost_sfx.set_volume(config.volume)
+	life_gain_sfx.set_volume(config.volume) 
 	game_over_sfx.set_volume(config.volume)
+	wipeout_sfx.set_volume(config.volume)
 
 def volume_mute():
 	if config.volume > 0.0:
@@ -33,7 +39,9 @@ def volume_mute():
 		switch_sfx.set_volume(config.volume)
 		selector_sfx.set_volume(config.volume)
 		life_lost_sfx.set_volume(config.volume)
+		life_gain_sfx.set_volume(config.volume) 
 		game_over_sfx.set_volume(config.volume)
+		wipeout_sfx.set_volume(config.volume)
 
 	else:
 		config.volume = 0.2
@@ -42,7 +50,9 @@ def volume_mute():
 		switch_sfx.set_volume(config.volume)
 		selector_sfx.set_volume(config.volume)
 		life_lost_sfx.set_volume(config.volume)
+		life_gain_sfx.set_volume(config.volume)
 		game_over_sfx.set_volume(config.volume)
+		wipeout_sfx.set_volume(config.volume)
 
 def volume_plus():
 	if config.volume == 1.0:
@@ -56,7 +66,9 @@ def volume_plus():
 		switch_sfx.set_volume(config.volume)
 		selector_sfx.set_volume(config.volume)
 		life_lost_sfx.set_volume(config.volume)
+		life_gain_sfx.set_volume(config.volume) 
 		game_over_sfx.set_volume(config.volume)
+		wipeout_sfx.set_volume(config.volume)
 
 def volume_minus():
 	if config.volume == 0.0:
@@ -70,4 +82,6 @@ def volume_minus():
 		switch_sfx.set_volume(config.volume)
 		selector_sfx.set_volume(config.volume)
 		life_lost_sfx.set_volume(config.volume)
+		life_gain_sfx.set_volume(config.volume) 
 		game_over_sfx.set_volume(config.volume)
+		wipeout_sfx.set_volume(config.volume)
